3e74c520b1f589745240c1b24292092264f851aa
jmcmicha <jmcmicha@com1636.eecs.utk.edu>
2022-07-14 15:13:13 -0400

    still need to move stuff to core

--- save_layout.c
+++ save_layout.c
@@ -12,8 +12,10 @@ static int         total_indx;
 static FILE       *fp;
 
 /* internal custom commands */
+static void _save_layout_cmd(int n_args, char **args);
 static void _frame_resize(int n_args, char **args);
-static void _frame_set_pos(int n_args, char **args);
+static void _frame_tree_resize(int n_args, char **args);
+static void _frame_tree_set_pos(int n_args, char **args);
 
 /* internal functions */
 static void _save_layout_unload(yed_plugin *self);
@@ -28,11 +30,17 @@ int yed_plugin_boot(yed_plugin *self) {
     Self = self;
 
     yed_plugin_set_command(self, "save-current-yed-layout", _save_current_yed_layout);
+    yed_plugin_set_command(self, "save-layout-cmd", _save_layout_cmd);
     yed_plugin_set_command(self, "frame-resize-cmdl", _frame_resize);
-    yed_plugin_set_command(self, "frame-set-pos", _frame_set_pos);
+    yed_plugin_set_command(self, "frame-tree-resize-cmdl", _frame_tree_resize);
+    yed_plugin_set_command(self, "frame-tree-set-pos", _frame_tree_set_pos);
 
-    if(yed_get_var("yed_layout_file") == NULL) {
-        yed_set_var("yed_layout_file", "");
+    if (yed_get_var("yed-layout-file") == NULL) {
+        yed_set_var("yed-layout-file", "");
+    }
+
+    if (yed_get_var("yed-layout-reload") == NULL) {
+        yed_set_var("yed-layout-reload", "no");
     }
 
     yed_plugin_set_unload_fn(self, _save_layout_unload);
@@ -56,13 +64,11 @@ static void _calculate_moves(int start, int finish, int total) {
 
     if (prev_moves >= next_moves) {
         for (int i = 0; i < next_moves; i++) {
-/*             yed_log("frame-next\n"); */
-            fprintf(fp, "frame-next\n");
+            fprintf(fp, "save-layout-cmd \"frame-next\"\n");
         }
     } else {
         for (int i = 0; i < prev_moves; i++) {
-/*             yed_log("frame-prev\n"); */
-            fprintf(fp, "frame-prev\n");
+            fprintf(fp, "save-layout-cmd \"frame-prev\"\n");
         }
     }
 }
@@ -81,11 +87,9 @@ static void _search(yed_frame_tree *curr_frame_tree, int current_indx) {
     _calculate_moves(command_indx, current_indx, total_indx);
 
     if (curr_frame_tree->split_kind == FTREE_VSPLIT) {
-/*         yed_log("frame-vsplit\n"); */
-        fprintf(fp, "frame-vsplit\n");
+        fprintf(fp, "save-layout-cmd \"frame-vsplit\"\n");
     } else {
-/*         yed_log("frame-hsplit\n"); */
-        fprintf(fp, "frame-hsplit\n");
+        fprintf(fp, "save-layout-cmd \"frame-hsplit\"\n");
     }
 
     command_indx  = total_indx;
@@ -113,12 +117,9 @@ static void _save_current_yed_layout(int n_args, char **args) {
     char             app[512];
     char            *path;
     frame_fixup     *f_fixup;
-    int              row;
-    int              col;
 
     if (!ys->options.no_init) {
         path = get_config_item_path("my_yed_layout.yedrc");
-/*         yed_log("path: %s\n", path); */
         fp = fopen (path, "w");
         free(path);
     }
@@ -127,16 +128,18 @@ static void _save_current_yed_layout(int n_args, char **args) {
         return;
     }
 
-/*     yed_log("start save\n"); */
-
     array_traverse(ys->frame_trees, root) {
         if ((*root) == yed_frame_tree_get_root(*root)) {
             command_indx = 0;
             total_indx   = 1;
             queue        = array_make(frame_fixup);
 
-/*             yed_log("frame-new\n"); */
-            fprintf(fp, "frame-new\n");
+            fprintf(fp, "save-layout-cmd \"frame-new\"\n");
+
+            fprintf(fp, "save-layout-cmd \"frame-tree-resize-cmdl %f %f\"\n", (*root)->height, (*root)->width);
+
+            fprintf(fp, "save-layout-cmd \"frame-tree-set-pos %f %f\"\n", (*root)->top, (*root)->left);
+
             _search(*root, 0);
 
             array_traverse(queue, f_fixup) {
@@ -144,29 +147,42 @@ static void _save_current_yed_layout(int n_args, char **args) {
                 command_indx = (*f_fixup).indx;
 
                 if ((*f_fixup).frame->name) {
-/*                     yed_log("frame-name %s\n", (*f_fixup).frame->name); */
-                    fprintf(fp, "frame-name %s\n", (*f_fixup).frame->name);
+                    fprintf(fp, "save-layout-cmd \"frame-name %s\"\n", (*f_fixup).frame->name);
                 }
 
                 if ((*f_fixup).frame->buffer) {
-                    yed_log("buffer %s\n", (*f_fixup).frame->buffer->name);
-                    fprintf(fp, "buffer %s\n", (*f_fixup).frame->buffer->name);
+                    fprintf(fp, "save-layout-cmd \"buffer %s\"\n", (*f_fixup).frame->buffer->name);
                 }
 
-/*                 yed_log("frame-move\n"); */
-/*                 fprintf(fp, "frame-move\n"); */
-
-                row = (*f_fixup).frame->bheight - ys->active_frame->bheight;
-                col = (*f_fixup).frame->bwidth - ys->active_frame->bwidth;
-                yed_log("frame-resize-cmdl %d %d\n", (*f_fixup).frame->bheight, (*f_fixup).frame->bwidth);
-                yed_log("frame-resize-cmdl %f %f\n", (*f_fixup).frame->height_f, (*f_fixup).frame->width_f);
-                fprintf(fp, "frame-resize-cmdl %f %f\n", (*f_fixup).frame->height_f, (*f_fixup).frame->width_f);
+                fprintf(fp, "save-layout-cmd \"frame-resize-cmdl %f %f\"\n", (*f_fixup).frame->height_f, (*f_fixup).frame->width_f);
             }
             array_free(queue);
         }
     }
 
     fclose(fp);
+}
+
+static void _save_layout_cmd(int n_args, char **args) {
+    int     i;
+    array_t split;
+
+    if (n_args != 1) {
+        yed_cerr("expected 1 argument, but got %d", n_args);
+        return;
+    }
+
+    if(yed_var_is_truthy("yed-layout-reload")) {
+        return;
+    }
+
+    for (i = 0; i < n_args; i += 1) {
+        split = sh_split(args[i]);
+        if (array_len(split)) {
+            yed_execute_command_from_split(split);
+        }
+        free_string_array(split);
+    }
 }
 
 static void _frame_resize(int n_args, char **args) {
@@ -185,16 +201,12 @@ static void _frame_resize(int n_args, char **args) {
     fheight = atof(args[0]);
     fwidth  = atof(args[1]);
 
-    yed_log("%f %f\n", fheight, fwidth);
-
     if (ys->active_frame &&
         ys->active_frame->tree &&
         ys->active_frame->tree->parent) {
 
         cur_r = fheight * ys->term_rows;
         cur_c = fwidth  * ys->term_cols;
-
-        yed_log("%d %d\n", cur_r, cur_c);
 
         row = cur_r - ys->active_frame->bheight;
         col = cur_c - ys->active_frame->bwidth;
@@ -215,9 +227,66 @@ static void _frame_resize(int n_args, char **args) {
     }
 }
 
-static void _frame_set_pos(int n_args, char **args) {
+static void _frame_tree_resize(int n_args, char **args) {
+    yed_frame_tree *root;
+    float fheight;
+    float fwidth;
+    int   root_r;
+    int   root_c;
+    int   cur_r;
+    int   cur_c;
+    int   row;
+    int   col;
 
+    if (n_args != 2) {
+        yed_cerr("expected 2 argument, but got %d", n_args);
+        return;
+    }
+
+    fheight = atof(args[0]);
+    fwidth  = atof(args[1]);
+
+    if (ys->active_frame) {
+        root  = yed_frame_tree_get_root(ys->active_frame->tree);
+        cur_r  = fheight * ys->term_rows;
+        cur_c  = fwidth  * ys->term_cols;
+        root_r = root->height * ys->term_rows;
+        root_c = root->width  * ys->term_cols;
+
+        row = cur_r - root_r;
+        col = cur_c - root_c;
+
+        yed_resize_frame_tree(root, row, col);
+    }
+}
+
+static void _frame_tree_set_pos(int n_args, char **args) {
+    float ftop;
+    float fleft;
+    int   cur_r;
+    int   cur_c;
+    int   row;
+    int   col;
+
+    if (n_args != 2) {
+        yed_cerr("expected 2 argument, but got %d", n_args);
+        return;
+    }
+
+    ftop  = atof(args[0]);
+    fleft = atof(args[1]);
+
+    if (ys->active_frame) {
+        cur_r = ftop  * ys->term_rows;
+        cur_c = fleft * ys->term_cols;
+
+        row = cur_r - ys->active_frame->top;
+        col = cur_c - ys->active_frame->left;
+
+        yed_move_frame(ys->active_frame, row, col);
+    }
 }
 
 static void _save_layout_unload(yed_plugin *self) {
+    yed_set_var("yed-layout-reload", "yes");
 }
